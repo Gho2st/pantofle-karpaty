5133b6725d6d97f440290ccac86b048e497efb0f
Dominik <dominik.jojczyk@gmail.com>
2025-10-19 13:20:33 +0200

    Update prisma.js

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,5 +1,8 @@
-// app/lib/prisma.js
+// lib/prisma.js
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 export default prisma;
